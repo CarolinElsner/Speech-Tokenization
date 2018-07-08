37fb3a248710f7342bf49c9e3b838edb4565e53d
teningnjie <tn043@hdm-stuttgart.de>
2018-07-08 23:32:37 +0200

    DetectApplication: Changes because of input change from Chunker to String sentence

--- SpeechTokenization/src/com/speechTokens/tokenizer/DetectApplication.java
+++ SpeechTokenization/src/com/speechTokens/tokenizer/DetectApplication.java
@@ -5,42 +5,34 @@ import java.util.ArrayList;
 public class DetectApplication {
 	// Die Klasse DetectApplication übernimmt die Suche nach Keywords bzgl. der Google Application
 	// Gefundene Keywords haben die Erstellung bzw. das Versenden von 
-	public static ArrayList<String> detection(Chunker chunks){
+	public static ArrayList<String> detection(String sentence){
 			
 			ArrayList<String> appdetection = new ArrayList<>();
-			chunks.printList();
-
+			System.out.println(sentence);
 // Zweidimesionales String Array zur Definition von Keywords --> {"Keyword", "zugeordneter Applikationstyp"}
 // Keywords sind stark vereinfacht definiert (Bsp. keep bezieht sich nicht nur auf die application) --> Ansatz: Möglichst viele Treffer
 // --> Jeden Nennung von "keep" --> ApplicationEvent auch wenn "keep" auch eine andere Bedeutung hat
 			
 
 			String[][] appkeywords = {
-					{"google mail","mail"},
-					{"mails","mail"},
 					{"mail","mail"},
-					{"google presentation","presentation"}, 
 					{"presentation", "presentation"},
-					{"presentations", "presentation"},
 					{"doc", "docs"},
-					{"docs", "docs"},
-					{"google docs","docs"},
 					{"google keep", "keep"},
-					{"notes", "keep"},
-					{"note", "note"},
+					{"note", "keep"},
+					{"google drive","drive"}
 					};
 
 // Übergebenes Chunker-Objekt wird durchlaufen und mit dem Array appkeywords verglichen.
 // Wird ein keyword gefunden, wird der ArrayList der zugehörige Apllikationstyp als neues Element hinzugefügt 		
 			
-			for(int i=0;i<chunks.size();i++) {
-				for(int j=0;j<appkeywords.length;j++) {
-					if(chunks.getChunkContentAt(i).equals(appkeywords[j][0])) {
-						appdetection.add(appkeywords[j][1]);
+			for(int i=0;i<appkeywords.length;i++) {
+					if(sentence.toLowerCase().contains(appkeywords[i][0])) {
+						appdetection.add(appkeywords[i][1]);
+						System.out.println("Application detected: " + appkeywords[i][1]);
 					}
 				}
-					
-			}			
+								
 
 			return appdetection;
 
