c7ff0b43905af7de942d3cdead85663c73fcf637
Benedikt <benegoos92@googlemail.com>
2018-07-16 19:47:25 +0200

    Zuschneiden der Applikationstypen für die GUI

--- SpeechTokenization/src/com/speechTokens/tokenizer/DetectApplication.java
+++ SpeechTokenization/src/com/speechTokens/tokenizer/DetectApplication.java
@@ -21,12 +21,20 @@ public class DetectApplication {
 //Iteration durch Array appkeywords. Prüfen, ob keywords im jeweiligen Satz enthalten sind.	
 // Wird ein keyword gefunden, wird der ArrayList der zugehörige Apllikationstyp als neues Element hinzugefügt 		
 			
-			//Interation durch alle Keyword und 
+			//Interation durch alle Keyword
 			
 			for(int i=0;i<appkeywords.size();i++) {
 					if(sentence.toLowerCase().contains(appkeywords.get(i))) {
+						//pr�fen ob Google in keyword enthalten ist, falls ja dann soll dies entfernt werden. Dient der besseren Anischt in der GUI
+						if(sentence.toLowerCase().contains("google")) {
+							String shortApplicationName = appkeywords.get(i).substring(7);
+							appdetection.add(shortApplicationName);
+							System.out.println("Application gefunden von Goolge " + shortApplicationName);
+						}
+						else {
 						appdetection.add(appkeywords.get(i));
 						System.out.println("Application detected: " + appkeywords.get(i));
+						}
 					}
 				}
 			return appdetection;
